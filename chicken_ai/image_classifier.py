43677e8488a5f886b45674b9d3517ef5d8504019
espin086 <jj.espinoza.la@gmail.com>
2023-12-11 20:52:12 -0800

    added project for chicken tracking

---
+++ chicken_ai/image_classifier.py
@@ -0,0 +1,14 @@
+def read_image_from_camera():
+    pass
+
+def label_objects_in_image():
+    pass
+
+def objects_of_interest():
+    pass
+
+def save_to_sqlite():
+    pass
+
+def visualize_data():
+    pass
